07194f12f1693ec6c494fe92a1b5828a194f6e67
Anita Savic <savicanita500@gmail.com>
2022-06-05 17:46:09 +0200

    Add Image

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -3,56 +3,62 @@ import styled from 'styled-components';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import 'animate.css';
+import Image from 'next/image';
 
 export default function LandingPage() {
 	const router = useRouter();
 	return (
-		<WrapperLandingPage>
-			<Link passHref href="/addBook">
-				<LinkHome aria-label="Add" pathName={router.pathname}>
-					<Button
-						type="button"
-						variant="Home"
-						className="animate__animated animate__fadeInLeft"
-					>
-						Add your Book
-					</Button>
-				</LinkHome>
-			</Link>
-			<Link passHref href="/liked">
-				<LinkHome aria-label="liked" pathName={router.pathname}>
-					<Button
-						type="button"
-						variant="Home"
-						className="animate__animated animate__fadeInRight"
-					>
-						Books I Loved
-					</Button>
-				</LinkHome>
-			</Link>
-			<Link passHref href="/disliked">
-				<LinkHome aria-label="disliked" pathName={router.pathname}>
-					<Button
-						ttype="button"
-						variant="Home"
-						className="animate__animated animate__fadeInLeft"
-					>
-						Books I Disliked
-					</Button>
-				</LinkHome>
-			</Link>
-			<Link passHref href="/tbr">
-				<LinkHome aria-label="toBeRead" pathName={router.pathname}>
-					<Button
-						type="button"
-						variant="Home"
-						className="animate__animated animate__fadeInRight"
-					>
-						My Future Reads
-					</Button>
-				</LinkHome>
-			</Link>
-		</WrapperLandingPage>
+		<>
+			<WrapperImage>
+				<Image src="/Books.jpg" alt="Bookshelf" layout="fill" />
+			</WrapperImage>
+			<WrapperLandingPage>
+				<Link passHref href="/addBook">
+					<LinkHome aria-label="Add" pathName={router.pathname}>
+						<Button
+							type="button"
+							variant="Home"
+							className="animate__animated animate__fadeInLeft"
+						>
+							Add your Book
+						</Button>
+					</LinkHome>
+				</Link>
+				<Link passHref href="/liked">
+					<LinkHome aria-label="liked" pathName={router.pathname}>
+						<Button
+							type="button"
+							variant="Home"
+							className="animate__animated animate__fadeInRight"
+						>
+							Books I Loved
+						</Button>
+					</LinkHome>
+				</Link>
+				<Link passHref href="/disliked">
+					<LinkHome aria-label="disliked" pathName={router.pathname}>
+						<Button
+							ttype="button"
+							variant="Home"
+							className="animate__animated animate__fadeInLeft"
+						>
+							Books I Disliked
+						</Button>
+					</LinkHome>
+				</Link>
+				<Link passHref href="/tbr">
+					<LinkHome aria-label="toBeRead" pathName={router.pathname}>
+						<Button
+							type="button"
+							variant="Home"
+							className="animate__animated animate__fadeInRight"
+						>
+							My Future Reads
+						</Button>
+					</LinkHome>
+				</Link>
+			</WrapperLandingPage>
+		</>
 	);
 }
 
@@ -62,4 +68,11 @@ export const LinkHome = styled.a`
 
 export const WrapperLandingPage = styled.div`
 	margin: 1rem;
+`;
+export const WrapperImage = styled.div`
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	z-index: -10;
+	position: fixed;
 `;
